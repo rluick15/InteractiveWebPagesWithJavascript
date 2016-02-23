de4b7e967729597670ae59d9cb2b10941ca8fe92
rluick15 <rluick15@gmail.com>
2016-02-22 20:31:40 -0500

    Initial Setup Commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTaskHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("complete-tasks");
+
+var addTask = function() {
+
+}
+
+var editTask = function() {
+
+}
+
+var deleteTask = function() {
+
+}
+
+var taskCompleted = function() {
+
+}
+
+var taskIncomplete = function() {
+
+}
+
