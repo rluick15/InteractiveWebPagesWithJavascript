a598a9dd7fa3fe7c236664dc391b0bc551b8a96c
rluick15 <rluick15@gmail.com>
2016-02-22 20:40:24 -0500

    Add click listener for Add Task Button

--- js/app.js
+++ js/app.js
@@ -4,22 +4,25 @@ var incompleteTaskHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("complete-tasks");
 
 var addTask = function() {
-
+    console.log("Add task");
 }
 
 var editTask = function() {
-
+    console.log("Edit task");
 }
 
 var deleteTask = function() {
-
+    console.log("Delete task");
 }
 
 var taskCompleted = function() {
-
+    console.log("Task incomplete");
 }
 
 var taskIncomplete = function() {
-
+    console.log("Task complete");
 }
+
+addButton.onclick = addTask;
+
 
